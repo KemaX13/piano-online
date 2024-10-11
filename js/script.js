85838f3c66df62a225f1c402036f366b944f56d6
Genta Mateo Kamekawa Roa <gentaroa1@gmail.com>
2024-10-11 09:02:45 +0200

    Añadido de función y eventos para la animación y sonido de las notas

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const keys = document.querySelectorAll(".key");
+
+function playNote(e) {
+    const noteValue = e.target.getAttribute('data-note');
+    const noteSound = new Audio("/sounds/" + noteValue + ".mp3");
+    
+    noteSound.play();
+    e.target.className += " no-border";
+}
+
+function resetNote(e) {
+    const noteValue = e.target.getAttribute('data-note');
+    const noteSound = new Audio("/sounds/" + noteValue + ".mp3");
+
+    noteSound.pause();
+    noteSound.currentTime = 0;
+    e.target.classList.remove("no-border");
+}
+
+keys.forEach(key => key.addEventListener("mousedown", playNote));
+keys.forEach(key => key.addEventListener("mouseup", resetNote));
