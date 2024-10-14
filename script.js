b21e800209788825f5a6d13eb2fcdfb041903a72
cristina romero <cris@MacBook-Air.home>
2024-10-14 17:04:53 +0200

    funcionamiento ampliar cursos terminado

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 let bloqueCurso = document.getElementById("piano-class-btn");
-
 function agrandarCurso() {
-    bloqueCurso.style.transform = "scale(1.5)"; 
+    bloqueCurso.style.transform = "scale(1.1)"; 
 }
-
 function reducirCurso() {
     bloqueCurso.style.transform = "scale(1)";
 }
-
 bloqueCurso.addEventListener('mouseover', agrandarCurso); 
 bloqueCurso.addEventListener('mouseout', reducirCurso);
 
+
+
+let bloqueCursoGuitarra = document.getElementById("guitar-class-btn");
+function agrandarCursoGuitarra() {
+    bloqueCursoGuitarra.style.transform = "scale(1.1)"; 
+}
+function reducirCursoGuitarra() {
+    bloqueCursoGuitarra.style.transform = "scale(1)";
+}
+bloqueCursoGuitarra.addEventListener('mouseover', agrandarCursoGuitarra); 
+bloqueCursoGuitarra.addEventListener('mouseout', reducirCursoGuitarra);
+
+
+let bloqueCursoFlauta = document.getElementById("flute-class-btn");
+function agrandarCursoFlauta() {
+    bloqueCursoFlauta.style.transform = "scale(1.1)"; 
+}
+
+function reducirCursoFlauta() {
+    bloqueCursoFlauta.style.transform = "scale(1)";
+}
+
+bloqueCursoFlauta.addEventListener('mouseover', agrandarCursoFlauta); 
+bloqueCursoFlauta.addEventListener('mouseout', reducirCursoFlauta);
