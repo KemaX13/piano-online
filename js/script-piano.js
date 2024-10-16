ce11fe745cad7e17cbdbdc1d834a671fff240f2a
Genta Mateo Kamekawa Roa <gentaroa1@gmail.com>
2024-10-16 12:58:38 +0200

    Quitar notas, arreglar sonido, cambiar nombre de carpeta imgcris y empezar responsive con movil

--- js/script-piano.js
+++ js/script-piano.js
@@ -1,59 +1,74 @@
+// const keys = document.querySelectorAll(".key");
+
+// function playNote(noteValue, isDown) {
+//   const noteSound = new Audio("/sounds/studio-grand-" + noteValue + ".mp3");
+  
+//   if(isDown) {
+//     noteSound.play();
+//     e.target.classList.add("no-border");
+//   } else {
+//     noteSound.pause();
+//     noteSound.currentTime = 0;
+//     e.target.classList.remove("no-border");
+//   }
+// }
+
+// function playNoteMouse(e, isDown) {
+//   const noteValue = e.target.getAttribute("data-note");
+//   playNote(noteValue, isDown);
+// }
+
+// function playNoteKey(e, isDown) {
+//   const noteValue = e.keyCode;
+  
+//   for(var i = 0; i < keys.length; i++) {
+//     if(keys[i].getAttribute("data-key") == noteValue) {
+//       playNote(keys[i].getAttribute("data-note"), isDown);
+//       break;
+//     }
+//   }
+// }
+
+// keys.forEach(key => {
+//   key.addEventListener("mousedown", (e) => playNoteMouse(e, true));
+//   key.addEventListener("mouseup", (e) => playNoteMouse(e, false));
+// });
+
+// window.addEventListener("keydown", (e) => playNoteKey(e, true));
+
 const keys = document.querySelectorAll(".key");
-const noteSounds = {};
-const keyMap = {};
-let currentOctave = 3;
+const keyMap = Object.fromEntries(
+  Array.from(keys).map(key => [key.dataset.key, key.dataset.note])
+);
 
-keys.forEach((key, i) => {
-  const dataNote = key.getAttribute("data-key");
-  keyMap[dataNote] = key.getAttribute("data-note");
-});
+const activeNotes = new Set();
 
-function playNote(noteValue, isDown) {
-  if (!noteSounds[noteValue])
-    noteSounds[noteValue] = new Audio("/sounds/studio-grand-" + noteValue + ".mp3");
+function playNote(noteValue, isDown, element) {
+  if (isDown && activeNotes.has(noteValue)) return;
+  const noteSound = new Audio(`/sounds/studio-grand-${noteValue}.mp3`);
   
   if (isDown) {
-    noteSounds[noteValue].play();
+    activeNotes.add(noteValue);
+    noteSound.play();
+    element.classList.add("no-border");
   } else {
-    noteSounds[noteValue].pause();
-    noteSounds[noteValue].currentTime = 0;
+    activeNotes.delete(noteValue);
+    element.classList.remove("no-border");
   }
 }
 
-function playNoteMouse(e, isDown) {
-  const noteValue = e.target.getAttribute("data-note");
+function handleNoteEvent(e, isDown) {
+  const noteValue = e.type.startsWith('mouse') ? 
+    e.target.dataset.note : 
+    keyMap[e.keyCode];
   
-  if (noteValue) {
-    playNote(noteValue, isDown);
-  } else {
-    console.error("Elemento no tiene atributo data-note");
-  }
-
-  if(isDown) {
-    e.target.className += " no-border";
-  } else {
-    e.target.classList.remove("no-border");
-  }
-}
-
-function playNoteKey(e, isDown) {
-  const noteValue = e.keyCode;
-  if (keyMap[noteValue]) {
-    playNote(keyMap[noteValue], isDown);
-  }
-}
-
-function changeOctave(i) {
-  currentOctave += i;
+  if (noteValue) playNote(noteValue, isDown, e.target);
 }
 
 keys.forEach(key => {
-  key.addEventListener("mousedown", (e) => playNoteMouse(e, true));
-  key.addEventListener("mouseup", (e) => playNoteMouse(e, false));
+  key.addEventListener("mousedown", e => handleNoteEvent(e, true));
+  key.addEventListener("mouseup", e => handleNoteEvent(e, false));
 });
 
-window.addEventListener("keydown", (e) => {
-  if (e.shiftKey) changeOctave(-1);
-  else if (e.key === 'Tab') changeOctave(1);
-  else  playNoteKey(e, true);
-});
+window.addEventListener("keydown", e => handleNoteEvent(e, true));
+window.addEventListener("keyup", e => handleNoteEvent(e, false));
